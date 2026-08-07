abc27b7b156b917d7641fc1078dbdf57262baecd
lxakshaseth <lxakshatseth90@gmail.com>
2026-08-07 12:42:14 +0530

    investion study

---
+++ Day1/research.js
@@ -0,0 +1,18 @@
+let score = "44ghg"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof  valueInNumber );
+console.log(valueInNumber );
+
+
+/*
+33 =>33
+43bnhjbfc = > NAN
+TRUE = 1 and false = 0
+
+
+
+*/
