9b05eb07684f942ab7ca4c48e7177157dbe7f30a
lxakshaseth <lxakshatseth90@gmail.com>
2026-08-08 13:11:43 +0530

    learn about comaprison operator

---
+++ Day2/operation.js
@@ -0,0 +1,67 @@
+let value = 3;
+let negval = -value
+// console.log(negval);
+
+/*arithmetic operations 
+console.log(2+2);
+console.log(2-1);
+console.log(2*2);
+console.log(2**3);
+console.log(2/4);
+console.log(2%4);
+
+*/
+
+
+/*concatenation in js 
+ let str1 = "akshat"
+ let str2 = " seth"
+ let str3 = str1 + str2
+ console.log(str3);
+
+//  */
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log(1+2+"2");
+//  console.log("1"+2+3);
+
+
+// console.log(+true);
+// console.log(true);
+
+// let gameCounter = 100;
+// gameCounter++;
+// console.log(gameCounter);
+
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// // Expected output: "a:4, b:4"
+
+
+
+//comparision
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+console.log(undefined === 0);
+console.log("3" === 3);
