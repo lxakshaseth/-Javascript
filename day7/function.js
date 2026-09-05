f141bee917e9e907849f99057970f2a31bc9a317
Akshat Gupta <lxakshatsethseth90@gmail.com>
2026-09-05 13:52:57 +0530

    learn what is function in js

---
+++ day7/function.js
@@ -0,0 +1,30 @@
+console.log("G");
+console.log("W");
+console.log("Q");
+console.log("W");
+console.log("R");
+console.log("F");
+console.log("F");
+console.log("D");
+
+console.log("next line");
+
+function myFunction() {
+    console.log("G");
+    console.log("W");
+    console.log("Q");
+    console.log("W");
+    console.log("R");
+    console.log("F");
+    console.log("F");
+    console.log("D");
+}
+
+myFunction();
+
+//add two numbers
+function addTwonumbers(number1, number2){
+    console.log(number1+number2);
+
+}
+addTwonumbers(40,90);
