d64ea8e130dcd41eec1a27966283cd5cc9cd2e93
Akshat Gupta <lxakshatsethseth90@gmail.com>
2026-09-05 14:07:16 +0530

    learn in deept concept of function

--- day7/function.js
+++ day7/function.js
@@ -20,11 +20,42 @@ function myFunction() {
     console.log("D");
 }
 
-myFunction();
+// myFunction();
 
-//add two numbers
+// //add two numbers
+// function addTwonumbers(number1, number2){
+//     console.log(number1+number2);
+
+// }
+// addTwonumbers(40,90);
+// addTwonumbers(40,"4");
+// addTwonumbers("4",9);
+
+
+myFunction ();
 function addTwonumbers(number1, number2){
-    console.log(number1+number2);
+    //let result = number1 + number2;
+    console.log("all the code before return statement will be executed");
+    //return result;
 
+    return number1 + number2;
+
+    // after return statement no code will be executed
+    consoloe.log("this is after return statement");
 }
-addTwonumbers(40,90);
+
+const result = addTwonumbers(40,90);
+console.log("result :" , result);
+
+
+console.log("loginUserMessage");
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+
+console.log(loginUserMessage("Gaurav"))
+
+console.log(loginUserMessage())
+//h
+//when we don't pass any value to the function then it will return undefined
