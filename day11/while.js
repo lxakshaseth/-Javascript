80b3bb2caa75c3694d37a7d5e6fef0fcf7fe3f87
Akshat Gupta <lxakshatsethseth90@gmail.com>
2026-09-14 16:08:30 +0530

    learn do while loop

--- day11/while.js
+++ day11/while.js
@@ -1,28 +1,44 @@
-// Initialize index with value 0
-let index = 0;
+// // Initialize index with value 0
+// let index = 0;
 
-// Run the loop while index is less than or equal to 10
-while (index <= 10) {
+// // Run the loop while index is less than or equal to 10
+// while (index <= 10) {
 
-    // Print the current value of index
-    console.log(`value of index is ${index}`);
+//     // Print the current value of index
+//     console.log(`value of index is ${index}`);
 
-    // Increase index by 5
-    index = index + 5;
-}
+//     // Increase index by 5
+//     index = index + 5;
+// }
 
-// Create an array containing superhero names
-let myArray = ['flash', 'batman', 'superman'];
+// // Create an array containing superhero names
+// let myArray = ['flash', 'batman', 'superman'];
 
-// Initialize arr with value 0 to access array elements
-let arr = 0;
+// // Initialize arr with value 0 to access array elements
+// let arr = 0;
 
-// Run the loop while arr is less than the array length
-while (arr < myArray.length) {
+// // Run the loop while arr is less than the array length
+// while (arr < myArray.length) {
 
-    // Print the current array element
-    console.log(`value is ${myArray[arr]}`);
+//     // Print the current array element
+//     console.log(`value is ${myArray[arr]}`);
 
-    // Move to the next array index
-    arr = arr + 1;
-}
+//     // Move to the next array index
+//     arr = arr + 1;
+// }   
+
+
+//do while loop
+// Initialize score with value 1
+let score = 1;
+
+// Start the do-while loop
+do {
+    // Print the current value of score
+    console.log(`Score is ${score}`);
+
+    // Increase score by 1
+    score++;
+
+// Continue the loop while score is less than or equal to 10
+} while (score <= 10);
